528975e3c39ed9a542b8bae7acc30604b61e3bbd
maram-mustafa <maram.w.mustafa@gmail.com>
2021-06-23 13:39:48 +0300

    fix

--- modules/mongooes-books.js
+++ modules/mongooes-books.js
@@ -1,16 +1,21 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
-const MONGODB_URI = process.env.MONGODB_URI
+const MONGODB_URI = process.env.MONGODB_URI;
 
 mongoose.connect(`${MONGODB_URI}/test`, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
+// mongoose.connect("mongodb://localhost:27017/test", {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
+
 const book = new mongoose.Schema({
   name: String,
   desc: String,
-  status:String,
+  status: String,
 });
 
 const owner = new mongoose.Schema({
@@ -28,13 +33,12 @@ function seedOwnerCollection() {
       {
         name: "The Candy Makers",
         desc: "This Book About CANDY",
-        status:  "life changing"      
-    
-    },
+        status: "life changing",
+      },
       {
         name: "My Little Golden Book About DOGS",
         desc: "This Book About Dogs",
-        status: " faviorite five"
+        status: " faviorite five",
       },
     ],
   });
@@ -44,18 +48,16 @@ function seedOwnerCollection() {
 }
 // seedOwnerCollection();
 
-
 function booksHandler(req, res) {
-    let email = req.query.email;
-  
-    Owners.find({ email: email }, (err, data) => {
-      err ? res.status(500).send("there is error") : res.send(data[0].books) ;
-    });
-  }
+  let email = req.query.email;
 
+  Owners.find({ email: email }, (err, data) => {
+    err ? res.status(500).send("there is error") : res.send(data[0].books);
+  });
+}
 
-  module.exports = {
-    booksHandler,
-    Owners,
-  }
+module.exports = {
+  booksHandler,
+  Owners,
+};
 ////
